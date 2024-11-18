77810de2771b8f1027ea4a73644ec00519e3dd38
isabelleribeiroo <00001113264251sp@al.educacao.sp.gov.br>
2024-11-18 17:00:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    'O que é um elemento químico?',
+    'Elemento químico é o conjunto de átomos de mesmo número atômico . O número atômico é a quantidade de prótons que um átomo possui em seu núcleo.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a força?',
+    'Uma força é uma interação que pode alterar o movimento, a direção ou a estrutura de um objeto.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Português',
+    'O que é uma antítese?',
+    'Duas palavras ou dois pensamentos de sentido contrário ex.: com luz no olhar e trevas no peito'
+)
